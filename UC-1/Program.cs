3c4f60407b610446b7817567bb1143aa6b0fb72f
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-27 14:11:57 +0530

    [Akanksha]: Linked List added

---
+++ UC-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UC_1
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LinkList list = new LinkList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+        }
+
+    }
+}
+
